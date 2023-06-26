1e2ea59900590f7f267dd69aeee50b1c22b581fc
MansiRavindraMohan <manasimohan29@gmail.com>
2023-06-26 12:12:27 +0530

    [Mansi]: Check whether two integers are equal or not

---
+++ Programming_Constructs/EqualNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Constructs
+{
+    public class EqualNumbers
+    {
+        public int num_One, num_Two;
+        public void CompareNumbers()
+        {
+            Console.Write("Enter first integer: ");
+            num_One = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second integer: ");
+            num_Two = Convert.ToInt32(Console.ReadLine());
+            if (num_One == num_Two)
+            {
+                Console.WriteLine("Both integers are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both integers are not equal");
+            }
+        }
+    }
+}
---
+++ Programming_Constructs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Constructs
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EqualNumbers equalNumbers = new EqualNumbers();
+            equalNumbers.CompareNumbers();
+            Console.ReadLine();
+        }
+    }
+}
